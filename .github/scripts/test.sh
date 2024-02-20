db5e99a256f065b5b116e1cf4887c80098035d54
J0hn-B <connect-wsman@hotmail.com>
2024-02-20 15:38:47 +0000

    add github workflows and taskfile

---
+++ .github/scripts/test.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# This script is used to hold all testing/linting functions for the project
+
+set -e
+
+# Bash function to run Super-Linter
+function gh_super_linter() {
+    # Start docker desktop if it's not running
+    source "$PWD/.github/scripts/server.sh"
+
+    # Run the docker container
+    docker run --rm \
+        -e RUN_LOCAL=true \
+        --env-file ".github/super-linter.env" \
+        -v "$PWD":/tmp/lint github/super-linter:slim-latest
+    echo
+}
+
+# Bash function to  run Checkov
+function checkov() {
+    # Start docker desktop if it's not running
+    source "$PWD/.github/scripts/server.sh"
+
+    # Run the docker container
+    docker run --rm \
+        -v "$PWD":/tmp/lint --workdir /tmp/lint \
+        bridgecrew/checkov \
+        --directory /tmp/lint \
+        --quiet
+    echo
+}
+
+# Bash function to run Trivy
+function trivy() {
+    # Start docker desktop if it's not running
+    source "$PWD/.github/scripts/server.sh"
+
+    # Run the docker container
+    docker run --rm \
+        -v "$PWD":/tmp/lint --workdir /tmp/lint \
+        aquasec/trivy:latest \
+        fs --scanners vuln,misconfig,secret . \
+        --skip-files ".env" \
+        --severity CRITICAL,HIGH,MEDIUM #,LOW # Adding LOW will return a list of files checked
+    echo
+}
+
+# Bash function to return the names of all the changed files
+function get_changed_files() {
+    # Get the list of changed files
+    git --no-pager diff --stat
+    echo
+}
